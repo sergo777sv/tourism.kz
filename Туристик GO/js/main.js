8685da5ae7458177598275d489fc6cbd277be428
sergo777sv <137034465+sergo777sv@users.noreply.github.com>
2023-06-19 12:19:09 +0600

    Add files via upload

---
+++ Туристик GO/js/main.js
@@ -0,0 +1,45 @@
+$(function(){
+    $('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+    $('.gallery__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+    });
+    $('.gallery__item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		}
+	});
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active')
+    })
+});
+
+$("img.logo__img").each(function () {
+    var $img = $(this);
+    var imgClass = $img.attr("class");
+    var imgURL = $img.attr("src");
+    $.get(imgURL, function (data) {
+        var $svg = $(data).find("svg");
+        if (typeof imgClass !== "undefined") {
+            $svg = $svg.attr("class", imgClass + " replaced-svg");
+        }
+        $svg = $svg.removeAttr("xmlns:a");
+        if (!$svg.attr("viewBox") && $svg.attr("height") && $svg.attr("width")) {
+            $svg.attr("viewBox", "0 0 " + $svg.attr("height") + " " + $svg.attr("width"))
+        }
+        $img.replaceWith($svg);
+    }, "xml");
+});
